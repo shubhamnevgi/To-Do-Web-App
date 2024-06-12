d504bd1d78e33c90a36613950ebd275f0c14258a
Shubham Nevgi <117476919+shubhamnevgi@users.noreply.github.com>
2024-06-12 12:58:08 +0530

    added service worker
    to provide offline access

---
+++ sw.js
@@ -0,0 +1,38 @@
+//*************sw.js*****************
+
+const staticCacheName = 'site-static';
+const assets = [
+  '/index.html',
+  '/js/script.js',
+  '/style.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+  'https://cdn-icons-png.flaticon.com/256/1187/1187525.png',
+  'https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap',
+];
+
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
